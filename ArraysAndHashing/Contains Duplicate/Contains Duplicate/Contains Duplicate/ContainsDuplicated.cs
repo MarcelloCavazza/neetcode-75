044534f768f527afa1b0500c53711556a98fbf66
Marcello Cavazza <marcellocavazzaoliveira@gmail.com>
2023-02-15 13:09:45 -0300

    first challenge

---
+++ ArraysAndHashing/Contains Duplicate/Contains Duplicate/Contains Duplicate/ContainsDuplicated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contains_Duplicate
+{
+    internal class ContainsDuplicated
+    {
+        public static Boolean ContainsDuplicate(int[] numberList)
+        {
+            for(int i = 0; i < numberList.Length; i++)
+            {
+
+            }
+            return true;
+        }
+    }
+}
---
+++ ArraysAndHashing/Contains Duplicate/Contains Duplicate/Contains Duplicate/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Contains_Duplicate;
+
+int[] intListFalse = new int[4] { 1, 2, 3, 4 };
+int[] intListTrue = new int[4] { 1, 2, 3, 4 };
+
+
+Boolean result1 = ContainsDuplicated.ContainsDuplicate(intListFalse);
+Boolean result3 = ContainsDuplicated.ContainsDuplicate(intListTrue);
