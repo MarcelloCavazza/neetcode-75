efc445e716f88dc228f4461c6af04283579a96ad
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-19 12:37:20 -0300

    making valid anagram, still to finish

---
+++ ArraysAndHashing/Valid Anagram/Valid Anagram/AnagramTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Anagram
+{
+    internal class AnagramTest
+    {
+        public static string firstTextIsAnagramOfTheSecondText(string originalString, string possibleString)
+        {
+            string originalStringTreated = originalString.Trim().ToLower();
+            string possibleStringTreated = possibleString.Trim().ToLower();
+            string[] possibleAnagramArray = possibleStringTreated.Split("");
+            if (possibleStringTreated == originalStringTreated) return "Same word";
+            for(int index = 0; index < possibleAnagramArray.Length; index++)
+            {
+                char storedLetter = Convert.ToChar(possibleAnagramArray[index]);
+                int indexOfLetter = Array.IndexOf(possibleAnagramArray, storedLetter);
+
+                string[] newArrayToTestNewPossibleAnagram = new string[possibleAnagramArray.Length];
+                
+            }
+        }
+    }
+}
---
+++ ArraysAndHashing/Valid Anagram/Valid Anagram/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Valid_Anagram;
+
+string originalString = "anagram";
+string possibleAnagram = "nagaram";
+
+string result = AnagramTest.firstTextIsAnagramOfTheSecondText(originalString, possibleAnagram);
+
+Console.WriteLine(result);  
